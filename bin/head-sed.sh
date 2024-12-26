d0a60b08a436582602a42821369c362922617ae1
aayo <aayo.software@gmail.com>
2024-12-26 15:21:37 -0800

    bash shell scripting code

--- bin/head-sed.sh
+++
@@ -1,8 +0,0 @@
-# head --- print first n lines
-#
-# usage: head N file
-count=$1
-sed ${count}q "$2"
-
-# When invoked as head 10 foo.xml, sed ends up being invoked as sed 10q foo.xml.
-# The q command causes sed to quit,immediately; no further input is read or commands executed.
--- bin/trace1.sh
+++
@@ -1,8 +0,0 @@
-# cat > trace1.sh Create script
-
-
-#! /bin/sh
-set -x #Turn on tracing
-echo 1st echo #Do something
-set +x #Turn off tracing
-echo 2nd echo
